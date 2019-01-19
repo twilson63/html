7507f7888c59b026ce7a1f240100a6b72e08a56c
Tom Wilson <twilson63@gmail.com>
2019-01-19 09:59:25 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# @twilson63/html
+
+create an index.html file from a cli command. 
+
+```
+npx @twilson63/html > index.html
+```
+
+LICENSE
+
+ISC
---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+console.log(`<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" >
+    <title>Title</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script src="./index.js"></script>
+  </body>
+</html>`)
+
